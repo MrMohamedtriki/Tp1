00274cbc32105b0411e1bf24554bcf3ac71e6cc8
MrMohamedtriki <mrmohamedtriki@gmail.com>
2024-02-18 23:45:45 +0100

    Update array value in test.php

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 echo "test";
-// $tab=array("Esya","assil","ayman","mohamed");
+// $tab=array("Esya","assil","ayman","mohamedddd");
 echo "<ul>";
 foreach($tab as $item){
     echo "<li>hello $item</li>";
